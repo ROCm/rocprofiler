add56e26aecb4fd68ef30b4472e06a4878d50a0a
Evgeny <evgeny.shcherbakov@amd.com>
2018-06-25 19:22:36 -0500

    license annotations
    Change-Id: I96b8f625a03b707b45aee6b8e7dbfabe073294c6

--- src/core/metrics.h
+++ src/core/metrics.h
@@ -1,3 +1,27 @@
+/******************************************************************************
+MIT License
+
+Copyright (c) 2018 ROCm Core Technology
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*******************************************************************************/
+
 #ifndef SRC_CORE_METRICS_H_
 #define SRC_CORE_METRICS_H_
 
@@ -126,7 +150,7 @@ class MetricsDict {
           const std::size_t pos2 = block_name.find(']');
           if (pos2 == std::string::npos) EXC_RAISING(HSA_STATUS_ERROR, "Malformed metric name '" << name << "'");
           block_name = name.substr(0, pos1);
-          const std::string block_index_str = name.substr(pos1 + 1, pos2 - (pos1 + 1)); 
+          const std::string block_index_str = name.substr(pos1 + 1, pos2 - (pos1 + 1));
           block_index = atol(block_index_str.c_str());
           indexed = true;
         }
