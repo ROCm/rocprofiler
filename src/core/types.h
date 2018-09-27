f76f8026c6de106d3870f483405867e16adef2ba
Evgeny <evgeny.shcherbakov@amd.com>
2018-09-27 17:22:46 -0500

    minor fix, hsa_ven_amd_aqlprofile_pfn_t alias
    Change-Id: I4f712cf39e577498da25e37323fcf847e39bfa19
    
    
    [ROCm/rocprofiler commit: 3443b09c49e56cf16830f9f95c34edff984a18e8]

--- src/core/types.h
+++ src/core/types.h
@@ -26,7 +26,7 @@ THE SOFTWARE.
 #include <hsa_ven_amd_aqlprofile.h>
 
 namespace rocprofiler {
-typedef hsa_ven_amd_aqlprofile_1_00_pfn_t pfn_t;
+typedef hsa_ven_amd_aqlprofile_pfn_t pfn_t;
 typedef hsa_ven_amd_aqlprofile_event_t event_t;
 typedef hsa_ven_amd_aqlprofile_parameter_t parameter_t;
 typedef hsa_ven_amd_aqlprofile_profile_t profile_t;
