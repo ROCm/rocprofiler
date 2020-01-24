0d35ec3acd180518b7f9427c212bc933c46406ab
Evgeny <evgeny.shcherbakov@amd.com>
2020-01-23 21:50:51 -0600

    roctx: range begin missed error
    Change-Id: I0e15d9a5642a840b6b0f82bbc78f16c4b019c553

--- bin/tblextr.py
+++ bin/tblextr.py
@@ -237,7 +237,9 @@ def fill_ext_db(table_name, db, indir, trace_name, api_pid):
           continue
 
         if cid == 2:
+          if not pid in range_stack: fatal("ROCTX range begin missed, pid(" + pid + ")");
           pid_stack = range_stack[pid]
+          if not tid in pid_stack: fatal("ROCTX range begin missed, tid(" + tid + ")");
           rec_stack = pid_stack[tid]
           rec_vals = rec_stack.pop()
           rec_vals[1] = tms
