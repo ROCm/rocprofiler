78e3e39e8fe6210e4ff271f6ef37592dd62666ae
Evgeny <evgeny.shcherbakov@amd.com>
2020-09-20 00:38:30 -0500

    enable contexts wait
    Change-Id: Ie2adf04662fddc8051fb5418904c9c659e264d78
    
    
    [ROCm/rocprofiler commit: 0d164ba672f834cbd9c070b989a07805ef084a4a]

--- src/core/rocprofiler.cpp
+++ src/core/rocprofiler.cpp
@@ -537,7 +537,8 @@ PUBLIC_API bool OnLoad(HsaApiTable* table, uint64_t runtime_version, uint64_t fa
     rocprofiler::StandaloneIntercept();
   }
 
-  ONLOAD_TRACE_END();
+  ONLOAD_TRACE("end intercept_mode(" << std::hex << intercept_mode << ")" <<
+               " intercept_mode_mask(" << std::hex << intercept_mode_mask << ")" << std::dec);
   return true;
 }
 
