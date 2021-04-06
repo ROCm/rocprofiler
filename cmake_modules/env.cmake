310e220c860825ff8a7bcb1eb0ab5fc739a3d61e
Rachida Kebichi <rachida.kebichi@amd.com>
2021-04-06 17:15:48 -0400

    SWDEV-272485 append to CMAKE_CXX_FLAGS instead of override
    Change-Id: I0c4364fa800a249d77ec7abd4e73556a8df1ef99
    
    
    [ROCm/rocprofiler commit: 429a458caed5bacd4c699b57be31590de0c9946d]

--- cmake_modules/env.cmake
+++ cmake_modules/env.cmake
@@ -36,7 +36,7 @@ add_definitions ( -DHSA_LARGE_MODEL= )
 add_definitions ( -DHSA_DEPRECATED= )
 
 ## Linux Compiler options
-set ( CMAKE_CXX_FLAGS "-std=c++11")
+set ( CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++11")
 set ( CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wall" )
 set ( CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Werror" )
 set ( CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Werror=return-type" )
