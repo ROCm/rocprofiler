0782afed3f3cf4808ca32cbe45b8e4bb33050f85
yoann-heitz <yoann.heitz@polymtl.ca>
2021-09-23 11:03:30 -0400

    Move kernel_trace_entry_t to rocprofiler_trace_entries.h

--- test/tool/tool.cpp
+++ test/tool/tool.cpp
@@ -376,30 +376,6 @@ void output_group(const context_entry_t* entry, const char* label) {
   }
 }
 
-struct kernel_trace_entry_t {
-  uint32_t dispatch;
-  uint32_t gpu_id;
-  uint32_t queue_id;
-  uint64_t queue_index;
-  uint32_t pid;
-  uint32_t tid;
-  uint32_t grid_size;
-  uint32_t workgroup_size;
-  uint32_t lds_size;
-  uint32_t scratch_size;
-  uint32_t vgpr;
-  uint32_t sgpr;
-  uint32_t fbarrier_count;
-  uint64_t signal_handle;
-  uint64_t object;
-  const char* kernel_name;
-  bool record;
-  uint64_t dispatch_time;
-  uint64_t begin;
-  uint64_t end;
-  uint64_t complete;
-};
-
 void kernel_flush_cb(kernel_trace_entry_t* entry){
   fprintf(result_file_handle, "dispatch[%u], gpu-id(%u), queue-id(%u), queue-index(%lu), pid(%u), tid(%u), grd(%u), wgr(%u), lds(%u), scr(%u), vgpr(%u), sgpr(%u), fbar(%u), sig(0x%lx), obj(0x%lx), kernel-name(\"%s\")",
     entry->dispatch,
