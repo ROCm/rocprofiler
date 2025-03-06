614f092da80cbaae2abd5fcb952270e34121d2b4
Giovanni Baraldi <gbaraldi@amd.com>
2025-03-06 18:12:00 +0100

    Adding AMDGPU_HSA_V6

--- plugin/att/disassembly.cpp
+++ plugin/att/disassembly.cpp
@@ -303,7 +303,8 @@ std::optional<uint64_t> DisassemblyInstance::va2fo(uint64_t va)
 
   CHECK_VA2FO(
     e_ident[EI_ABIVERSION] == 2 || // ELFABIVERSION_AMDGPU_HSA_V4
-    e_ident[EI_ABIVERSION] == 3, "unexpected ei_abiversion"); // ELFABIVERSION_AMDGPU_HSA_V5
+    e_ident[EI_ABIVERSION] == 3 || // ELFABIVERSION_AMDGPU_HSA_V5
+    e_ident[EI_ABIVERSION] == 4, "unexpected ei_abiversion"); // ELFABIVERSION_AMDGPU_HSA_V6
 
   Elf64_Ehdr *ehdr = (Elf64_Ehdr*)buffer.data();
   CHECK_VA2FO(ehdr, "ehdr is nullptr");
